549f0bbf4610b2c105c752f3daf4eac483db9f08
Animesh <animesh489@gmail.com>
2023-10-09 20:23:30 +0530

    first setup

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ResponsiveAppBar from './components/Appbar'; // Ensure the correct import name
+import Sidenav from './components/Sidenav'; // Ensure the correct import name
+import Home from './pages/Home'; // Ensure the correct import name
+import About from './pages/About'; // Ensure the correct import name
+
+function App(): JSX.Element {
+  return (
+
+    <div>
+      <ResponsiveAppBar />
+      <div className="container">
+        <Sidenav />
+
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
